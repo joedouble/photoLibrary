03c7faa49f4a610f24ee259ebcd2ad679a75e924
Joseph Double <josephdouble@Josephs-MacBook-Pro.local>
2019-02-26 21:22:25 -0500

    5 nonrandom pictures on home page.

--- src/main/java/com/detroitlabs/photolibrary/controller/PhotoController.java
+++ src/main/java/com/detroitlabs/photolibrary/controller/PhotoController.java
@@ -1,14 +1,25 @@
 package com.detroitlabs.photolibrary.controller;
 
+import com.detroitlabs.photolibrary.data.PhotoRepository;
+import com.detroitlabs.photolibrary.model.Photo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class PhotoController {
 
+    @Autowired
+    private PhotoRepository photoRepository;
+
     @RequestMapping("/")
-    public String displayPhotos(){
+    public String displayPhotos(ModelMap modelMap){
+        List<Photo> allPhotos = photoRepository.random5Photos();
+        modelMap.put("allPhotos", allPhotos);
         return "home";
     }
 }
--- src/main/java/com/detroitlabs/photolibrary/data/PhotoRepository.java
+++ src/main/java/com/detroitlabs/photolibrary/data/PhotoRepository.java
@@ -3,6 +3,8 @@ package com.detroitlabs.photolibrary.data;
 import com.detroitlabs.photolibrary.model.Photo;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,6 +12,30 @@ import java.util.List;
 public class PhotoRepository {
 
     private static final List<Photo> ALL_PHOTOS = Arrays.asList(
-            new Photo()
-    );
+            new Photo("andromeda", LocalDate.of(2019, 2, 26), "andromeda, ascendant"),
+            new Photo("rocinante", LocalDate.of(2019, 2, 26), "corvette class, expanse"),
+            new Photo("oneill", LocalDate.of(2019, 2, 26), "daedalus class, stargate atlantic"),
+            new Photo("enterprise", LocalDate.of(2019, 2, 26), "galaxy class, star trek"),
+            new Photo("voyager", LocalDate.of(2019, 2, 26), "intrepid class, star trek"),
+            new Photo("galactica", LocalDate.of(2019, 2, 26), "jupiter class, battlestar galactica"),
+            new Photo("nauvoo", LocalDate.of(2019, 2, 26), "mormon generation ship, expanse"),
+            new Photo("infinity", LocalDate.of(2019, 2, 26), "unsc infinity class, halo"),
+            new Photo("discovery", LocalDate.of(2019, 2, 26), "crossfield class, star trek"),
+            new Photo("pegasus", LocalDate.of(2019, 2, 26), "mercury class, battlestar galactica")
+            );
+
+    public List<Photo> getAllPhotos() {
+        return ALL_PHOTOS;
+    }
+
+    public List<Photo> random5Photos(){
+
+        List<Photo> random5 =  new ArrayList<>();
+
+        for (int i = 0; i < 5; i++){
+            random5.add(ALL_PHOTOS.get(i));
+        }
+        return random5;
+    }
+
 }
